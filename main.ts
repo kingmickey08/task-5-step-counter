2cfa0a9433a02849183d1f5af09bbc11ce6bf030
kingmickey08 <81299672+kingmickey08@users.noreply.github.com>
2022-09-28 14:34:24 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    step = 0
+})
 input.onGesture(Gesture.Shake, function () {
     step += 1
-    basic.showNumber(step)
 })
 let step = 0
 step = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        step = 0
-    }
+    basic.showNumber(step)
 })
